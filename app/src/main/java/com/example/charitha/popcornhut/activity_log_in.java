c62267e2a9db41f001507b2723873b58ae245e18
Charitha Wickramanayake <charitha.wickramanayake@gmail.com>
2017-07-18 12:11:19 +0530

    18th second

--- app/src/main/java/com/example/charitha/popcornhut/activity_log_in.java
+++ app/src/main/java/com/example/charitha/popcornhut/activity_log_in.java
@@ -1,13 +1,43 @@
 package com.example.charitha.popcornhut;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class activity_log_in extends AppCompatActivity {
+
+    public Button signupBtn;
+
+    public void init(){
+
+        signupBtn = (Button)findViewById(R.id.button4);
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent signUp = new Intent(activity_log_in.this, sign_up.class);
+
+                startActivity(signUp);
+
+            }
+        });
+
+    }
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        init();
+
+
     }
 }
